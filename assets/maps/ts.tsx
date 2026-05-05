180d0a25408cd6ecfa30e4b48743cac27700ec15
Gilgamesh64 <chto24io@gmail.com>
2026-05-05 11:44:28 +0200

    better packing and bridge

--- assets/maps/ts.tsx
+++ assets/maps/ts.tsx
@@ -1,9 +1,85 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="ts" tilewidth="32" tileheight="32" tilecount="32" columns="32">
- <image source="../packed/tileset.png" width="1024" height="32"/>
+<tileset version="1.10" tiledversion="1.12.1" name="ts" tilewidth="32" tileheight="32" tilecount="27" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="tileset/corner_of_grass_down_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="tileset/corner_of_grass_down_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="tileset/corner_of_grass_up_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="tileset/corner_of_grass_up_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="tileset/grass.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="tileset/grass_surrounding_path_curve_corner_down_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="6">
+  <image source="tileset/grass_surrounding_path_curve_corner_down_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="7">
+  <image source="tileset/grass_surrounding_path_curve_corner_up_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="tileset/grass_surrounding_path_curve_corner_up_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="9">
+  <image source="tileset/half_grass_down.png" width="32" height="32"/>
+ </tile>
+ <tile id="10">
+  <image source="tileset/half_grass_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="11">
+  <image source="tileset/half_grass_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="12">
+  <image source="tileset/half_grass_up.png" width="32" height="32"/>
+ </tile>
+ <tile id="13">
+  <image source="tileset/daisies_down_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="14">
+  <image source="tileset/daisies_down_left_corner.png" width="32" height="32"/>
+ </tile>
+ <tile id="15">
+  <image source="tileset/daisies_down_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="tileset/daisies_down_right_corner.png" width="32" height="32"/>
+ </tile>
+ <tile id="17">
+  <image source="tileset/daisies_up_left.png" width="32" height="32"/>
+ </tile>
+ <tile id="18">
+  <image source="tileset/daisies_up_left_corner.png" width="32" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="tileset/daisies_up_right.png" width="32" height="32"/>
+ </tile>
+ <tile id="20">
+  <image source="tileset/daisies_up_right_corner.png" width="32" height="32"/>
+ </tile>
+ <tile id="21">
+  <image source="tileset/bridge.png" width="32" height="32"/>
+ </tile>
+ <tile id="22">
+  <image source="tileset/path.png" width="32" height="32"/>
+ </tile>
+ <tile id="23">
+  <image source="tileset/rock.png" width="32" height="32"/>
+ </tile>
  <tile id="24">
-  <properties>
-   <property name="blocked" type="bool" value="true"/>
-  </properties>
+  <image source="tileset/rock_with_grass.png" width="32" height="32"/>
+ </tile>
+ <tile id="25">
+  <image source="tileset/unwalkable_terrain.png" width="32" height="32"/>
+ </tile>
+ <tile id="26">
+  <image source="tileset/water.png" width="32" height="32"/>
  </tile>
 </tileset>
